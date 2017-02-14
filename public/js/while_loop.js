13a174a1b4119374b8e862e6ad564d4c0bf0ad8b
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-14 14:42:51 -0600

    completed while loop

---
+++ public/js/while_loop.js
@@ -0,0 +1,34 @@
+/* 'use strict';
+
+var i = 2;
+
+while (i <= 32768) {
+	i*=2;
+	console.log(i);
+}
+*/
+
+'use strict';
+
+// This is how you get a random number between 50 and 100
+var coneInventory = Math.floor(Math.random() * 50) + 50;
+
+
+do {
+	//generate a random number of cones to sell
+	console.log('while loop iteration #' + i);
+    i++;
+	//if we have that many left, subtract it from our inventory
+} while (i < 10);
+
+var cones = Math.floor(Math.random() * 5) + 1;
+
+
+
+    
+/*} while (i < 10);
+
+
+
+// This is how you get a random number between 1 and 5
+*/
