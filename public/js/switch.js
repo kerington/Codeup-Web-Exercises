5e123867138d035f54b2eb7c98063e3218628c3c
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-14 14:41:56 -0600

    completed switch js

---
+++ public/js/switch.js
@@ -0,0 +1,69 @@
+/*
+~~WALMART DISCOUNT -- CHECK SLACK FOR JUSTINS VERSION! THEN PRACTICE IT HIS WAY--
+*/
+// var luckyNumber = Math.floor(Math.random()* 6);
+
+// switch (luckyNumber) {
+//     case 1:
+//         console.log("10% off $60.00 = $54.00");
+//         break;
+//     case 2:
+//         console.log("25% off $60.00 = $45.00");
+//         break;
+//     case 3:
+//         console.log("35% off $60.00 = $39.00");
+//         break;
+//     case 4:
+//         console.log("50% off $60.00 = $30.00");
+//         break;
+//     case 5:
+//         console.log("YOUR ORDER IS FREE");
+//         break;
+//     default:
+//         console.log("No Discount!");
+//     }
+ 
+
+
+// ~~MONTHS
+
+var Months = 1;
+switch (Months) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    }
+
