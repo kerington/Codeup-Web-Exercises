279ce59753c5cc18d4f0412f5b6ed3f5843f41d6
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-15 10:34:47 -0600

    addint For Loop exercise 2/15

---
+++ public/js/for_loop.js
@@ -0,0 +1,65 @@
+--
+
+/*NUMBERS OUTPUT*/
+
+// for (var i = 1; i <= 1; i++) {
+//     console.log('1');
+// }
+// for (var i = 1; i <= 2; i++) {
+//     console.log('2');
+// }
+// for (var i = 1; i <= 3; i++) {
+//     console.log('3');
+// }
+// for (var i = 1; i <= 4; i++) {
+//     console.log('4');
+// }
+// for (var i = 1; i <= 5; i++) {
+//     console.log('5');
+// }
+// for (var i = 1; i <= 6; i++) {
+//     console.log('6');
+// }
+// for (var i = 1; i <= 7; i++) {
+//     console.log('7');
+// }
+// for (var i = 1; i <= 8; i++) {
+//     console.log('8');
+// }
+// for (var i = 1; i <= 9; i++) {
+//     console.log('9');
+// }
+// for (var i = 1; i <= 10; i++) {
+//     console.log('0');
+// }
+
+--
+
+// /* RANDOM MULTIPLICATION TABLE */
+
+// var num = Math.floor(Math.random()* 10 + 1);
+// //use same logic from warmup 215 (math.random)
+ 
+// var num = parseInt(num); //parse the num to number
+// var i = 0;
+ 
+// for(i = 1; i <= 10; i++) {
+//     console.log(num + " x " + i + " = " + num * i );
+// }
+
+
+--
+
+/*RANDOM EVEN OR ODD*/
+
+var oddOrEven = Math.floor((Math.random()) * (200 - 20)) + 10; }
+
+	function oddOrEven(x) {
+	  return ( x & 1 ) ? "odd" : "even";
+	}
+
+
+
+
+
+
