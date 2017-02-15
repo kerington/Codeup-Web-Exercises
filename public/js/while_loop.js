aadd14b45f5dad7d697f07b5bde804928f019ff8
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-15 11:32:24 -0600

    saved file after cutting and pasting code to use for another exercise.

--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -1,12 +1,13 @@
-/* 'use strict';
+// 'use strict';
 
-var i = 2;
+// var i = 2;
 
-while (i <= 32768) {
-	i*=2;
-	console.log(i);
-}
-*/
+// while (i <= 32768) {
+// 	i = i * 2;
+// 	/*OR i *=2*/
+// 	console.log(i);
+// }
+
 
 'use strict';
 
@@ -15,13 +16,52 @@ var coneInventory = Math.floor(Math.random() * 50) + 50;
 
 
 do {
-	//generate a random number of cones to sell
-	console.log('while loop iteration #' + i);
-    i++;
-	//if we have that many left, subtract it from our inventory
-} while (i < 10);
+	var conesToSell = Math.floor(Math.random() * 5) + 1;
+	 // if we have that many left, subtract it from our inventory
+	if(conesToSell <= coneInventory) {
+		console.log('Sold' + conesToSell + ' cones...')
+		coneInventory = coneInventory - conesToSell;
+	} else {
+		console.log('I don\'t have enough!')
+	}
+while (coneInventory =0)
+	}
 
-var cones = Math.floor(Math.random() * 5) + 1;
+
+
+
+
+// WALMART CHECKOUT
+
+var luckyNumber = Math.floor(Math.random() * 6),
+	receipt = 60,
+	total = null;
+
+console.log("Your lucky number is: " + luckyNumber);
+
+switch (luckyNumber) {
+	case 0:
+		total = receipt;
+		break;
+	case 1:
+		total = receipt - (receipt * .1);
+		break;
+	case 2:
+		total = receipt - (receipt * .25);
+		break;
+	case 3:
+		total = receipt - (receipt * .35);
+		break;
+	case 4:
+		total = receipt - (receipt * .5);
+		break;
+	case 5:
+		total = 0;
+		break;
+	default:
+		console.log("Something went wrong.")
+}
+console.log("Your price is $" + total.toFixed(2));
 
 
 
