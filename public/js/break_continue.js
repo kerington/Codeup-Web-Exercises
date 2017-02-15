d8d2e2d164b1c7d04c0e7d21db7c95b21f845b24
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-15 14:46:29 -0600

    used code for warmup activity, saved by habit.

--- public/js/break_continue.js
+++ public/js/break_continue.js
@@ -1,28 +1,28 @@
 'use strict';
 
-// var num = Math.floor((Math.random()*50)+1);
-// 	console.log('Random number to skip is ' + num);
+var num = Math.floor((Math.random()*50)+1);
+	console.log('Random number to skip is ' + num);
 
-// 	for (var i = 1; i <= 50; i++) {
+	for (var i = 1; i <= 50; i++) {
 
-// 	    if (i % 2 == 0) {
-// 	        continue;
-// 	    }
+	    if (i % 2 == 0) {
+	        continue;
+	    }
 
-// 	    if (num === i) {
-// 	    	console.log('Yikes! Skipping number ' + i);
-// 	    } else {
-// 	    console.log('Here is an odd number: ' + i);
-// 	}
+	    if (num === i) {
+	    	console.log('Yikes! Skipping number ' + i);
+	    } else {
+	    console.log('Here is an odd number: ' + i);
+	}
 
-// 	if (i >50) {
-// 		break;
-// 	}
+	if (i >50) {
+		break;
+	}
 
-// 	    // console.log('Random odd number to skip is:' + i)
-// 		// 	break;
-// 		// console.log('meow');
-// 	}
+	    // console.log('Random odd number to skip is:' + i)
+		// 	break;
+		// console.log('meow');
+	}
 
 
 
