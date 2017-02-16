1b498b9096c654dafe7e95a74a35e3344722af8e
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-16 11:49:40 -0600

    adding warmups to git hub after completing them in class

--- public/js/warmuppt2215.js
+++ public/js/warmuppt2215.js
@@ -16,7 +16,7 @@ for (var i = 1; i <= 100; i+=1) {
 
         if ( i % 3 == 0 && i % 5 !=0 ) {
             console.log("Fizz");
-        } else if ( i % 5 == 0 && i % 3 !=0) {
+        } else if ( xi % 5 == 0 && i % 3 !=0) {
             console.log("Buzz");
         } else if ( i % 15 == 0) {
             console.log("FizzBuzz");
