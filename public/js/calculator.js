39848f7fcbbaece2815f3060d02ee493fe4976c4
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-21 15:41:22 -0600

    finished calculator exercise for the day

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -14,52 +14,86 @@ var middleInput = document.getElementById('boxMid');
 
 		console.log(event.target.innerText);
 	}
-	//FOR BOX MIDDLE
+		//FOR BOX MIDDLE
 	var operatorDisplay = function(event){
 		if (leftInput.innerText !== "") {
 			middleInput.innerText = event.target.innerText;
 		}
 	}
 
-	//TO CLEAR ALL
+		//TO CLEAR ALL
 	var clearAll = function(event){
 		leftInput.innerText = "";
 		middleInput.innerText = "";
 		rightInput.innerText = "";
 	}
 
-	var total = function(event){
+		//TO CHANGE TO NEG NUMBER
+	var switchSigns = function() {
 
+		if (middleInput.innerText === ""){
+  			leftInput.innerText *= -1;
+  		} else {
+  			rightInput.innerText *= -1;
+  		} 
+	}
+
+		//TO GET A PERCENT
+		var percent = function() {
+
+		if (middleInput.innerText === ""){
+  			leftInput.innerText /= 100;
+  		} else {
+  			rightInput.innerText /= 100;
+  		} 
+	}
+
+
+		//FOR TOTAL
+		var equals = function(event){
+		var total;
         switch(middleInput.innerText) {
 
 			case '+' : 
-				total = parseInt(leftInput.innerText) + parseInt(rightInput.innerText);
+				total = parseFloat(leftInput.innerText) + parseFloat(rightInput.innerText);
+				console.log(leftInput.innerText);
+				console.log(rightInput.innerText);
+				console.log(total);
+
+			break;
+
+			case '-' : 
+				total = parseFloat(leftInput.innerText) - parseFloat(rightInput.innerText);
 				console.log(leftInput.innerText);
 				console.log(rightInput.innerText);
 				console.log(total);
 			break;
 
-			case '-' : 
-				total = parseInt(leftInput.innerText) - parseInt(rightInput.innerText);
-				console.log(total);
-			break;
-
 			case '*' : 
-				total = parseInt(leftInput.innerText) * parseInt(rightInput.innerText);
+				total = parseFloat(leftInput.innerText) * parseFloat(rightInput.innerText);
+				console.log(leftInput.innerText);
+				console.log(rightInput.innerText);
 				console.log(total);
 			break;
 
 			case '/' : 
-				total = parseInt(leftInput.innerText) / parseInt(rightInput.innerText);
+				total = parseFloat(leftInput.innerText) / parseFloat(rightInput.innerText);
+				console.log(leftInput.innerText);
+				console.log(rightInput.innerText);
 				console.log(total);
 			break;
 
-			case 'boxNeg' : 
-				total = parseInt(leftInput.innerText) *-"1" parseInt(rightInput.value);
-			break;
+			// case '+/-' : 
+			// 	total = parseInt(leftInput.innerText) *(-1) parseInt(rightInput.innerText);
+			// break;
 		}
+		leftInput.innerText = total;
+		middleInput.innerText = "";
+		rightInput.innerText = "";
 	}
 
+
+		//ELEMENTS
 		document.getElementById('box0').addEventListener('click', listener);
 		document.getElementById('box1').addEventListener('click', listener);
 		document.getElementById('box2').addEventListener('click', listener);
@@ -72,32 +106,13 @@ var middleInput = document.getElementById('boxMid');
 		document.getElementById('box9').addEventListener('click', listener);
 		document.getElementById('boxDot').addEventListener('click', listener);
 
-
 		document.getElementById('boxAdd').addEventListener('click', operatorDisplay);
 		document.getElementById('boxSubt').addEventListener('click', operatorDisplay);
 		document.getElementById('boxMult').addEventListener('click', operatorDisplay);
 		document.getElementById('boxDiv').addEventListener('click', operatorDisplay);
-		document.getElementById('boxNeg').addEventListener('click', operatorDisplay);
-
+		document.getElementById('boxNeg').addEventListener('click', switchSigns);
 
 		document.getElementById('boxClr').addEventListener('click', clearAll);
-		document.getElementById('boxEq').addEventListener('click', total);
-
-
-	// TO CLEAR ????
-
-
-	// ALLOWING INPUT TO LEFT AND RIGHT BOXES
-	//NEEDS WORK!!!
+		document.getElementById('boxEq').addEventListener('click', equals);
+		document.getElementById('boxPerc').addEventListener('click',percent);
 		
-		
-
-		// var middleInput = document.getElementById('boxMid');
-		
-
-
-
-// clear sets every display to empty screen
-
-
-	//How to APPEND text!?????
