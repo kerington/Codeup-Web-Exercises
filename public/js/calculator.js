a0b7896b0b5af6112d600ad0fbeb53880bd2d43b
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-02-21 13:47:07 -0600

    saving calculator project after lunch break

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,9 +1,63 @@
 'use strict';
 
+var leftInput = document.getElementById('boxLeft');
+var rightInput = document.getElementById('boxRight');
+var middleInput = document.getElementById('boxMid');
 		// FOR BOX LEFT
-	var listener = function (event) {
+	var listener = function (event){
+
+		if (middleInput.innerText === ""){
+  			leftInput.innerText = leftInput.innerText + event.target.innerText;
+  		} else {
+  			rightInput.innerText = rightInput.innerText + event.target.innerText;
+  		} 
+
 		console.log(event.target.innerText);
-		leftInput.innerText = event.target.innerText;
+	}
+	//FOR BOX MIDDLE
+	var operatorDisplay = function(event){
+		if (leftInput.innerText !== "") {
+			middleInput.innerText = event.target.innerText;
+		}
+	}
+
+	//TO CLEAR ALL
+	var clearAll = function(event){
+		leftInput.innerText = "";
+		middleInput.innerText = "";
+		rightInput.innerText = "";
+	}
+
+	var total = function(event){
+
+        switch(middleInput.innerText) {
+
+			case '+' : 
+				total = parseInt(leftInput.innerText) + parseInt(rightInput.innerText);
+				console.log(leftInput.innerText);
+				console.log(rightInput.innerText);
+				console.log(total);
+			break;
+
+			case '-' : 
+				total = parseInt(leftInput.innerText) - parseInt(rightInput.innerText);
+				console.log(total);
+			break;
+
+			case '*' : 
+				total = parseInt(leftInput.innerText) * parseInt(rightInput.innerText);
+				console.log(total);
+			break;
+
+			case '/' : 
+				total = parseInt(leftInput.innerText) / parseInt(rightInput.innerText);
+				console.log(total);
+			break;
+
+			case 'boxNeg' : 
+				total = parseInt(leftInput.innerText) *-"1" parseInt(rightInput.value);
+			break;
+		}
 	}
 
 		document.getElementById('box0').addEventListener('click', listener);
@@ -16,66 +70,34 @@
 		document.getElementById('box7').addEventListener('click', listener);
 		document.getElementById('box8').addEventListener('click', listener);
 		document.getElementById('box9').addEventListener('click', listener);
+		document.getElementById('boxDot').addEventListener('click', listener);
 
 
-
-	// FOR BOX MID
-	var listener = function (event) {
-		console.log(event.target.innerText);
-		middleInput.innerText = event.target.innerText;
-	}
-
-		document.getElementById('boxAdd').addEventListener('click', listener);
-		document.getElementById('boxSubt').addEventListener('click', listener);
-		document.getElementById('boxMult').addEventListener('click', listener);
-		document.getElementById('boxDiv').addEventListener('click', listener);
-		document.getElementById('boxPrc').addEventListener('click', listener);
-		document.getElementById('boxPlMi').addEventListener('click', listener);
+		document.getElementById('boxAdd').addEventListener('click', operatorDisplay);
+		document.getElementById('boxSubt').addEventListener('click', operatorDisplay);
+		document.getElementById('boxMult').addEventListener('click', operatorDisplay);
+		document.getElementById('boxDiv').addEventListener('click', operatorDisplay);
+		document.getElementById('boxNeg').addEventListener('click', operatorDisplay);
 
 
-
-	// FOR BOX RIGHT IF/ELSE BOOLEAN CHECK!!!**
-	var listener = function (event) {
-		console.log(event.target.innerText);
-		rightInput.innerText = event.target.innerText;
-	}
-
-		document.getElementById('box0').addEventListener('click', listener);
-		document.getElementById('box1').addEventListener('click', listener);
-		document.getElementById('box2').addEventListener('click', listener);
-		document.getElementById('box3').addEventListener('click', listener);
-		document.getElementById('box4').addEventListener('click', listener);
-		document.getElementById('box5').addEventListener('click', listener);
-		document.getElementById('box6').addEventListener('click', listener);
-		document.getElementById('box7').addEventListener('click', listener);
-		document.getElementById('box8').addEventListener('click', listener);
-		document.getElementById('box9').addEventListener('click', listener);
-
-	// TO CLEAR 
+		document.getElementById('boxClr').addEventListener('click', clearAll);
+		document.getElementById('boxEq').addEventListener('click', total);
 
 
-		var leftInput = document.getElementById('boxLeft');
-		var middleInput = document.getElementById('boxMid');
-		var rightInput = document.getElementById('boxRight');
+	// TO CLEAR ????
 
 
-
+	// ALLOWING INPUT TO LEFT AND RIGHT BOXES
+	//NEEDS WORK!!!
+		
 		
 
+		// var middleInput = document.getElementById('boxMid');
+		
+
+
+
 // clear sets every display to empty screen
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	//How to APPEND text!?????
